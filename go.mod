d7566faf3ca9e2239adfc544b06e529fb6f6487a
salih bozkaya <bozkayasalih01x@gmail.com>
2022-12-31 16:33:30 +0300

    feat: added realtime playwright twitter twieet supoort

--- go.mod
+++ go.mod
@@ -2,10 +2,10 @@ module discord-bot
 
 go 1.19
 
-require github.com/bwmarrin/discordgo v0.26.1
+require github.com/playwright-community/playwright-go v0.2000.1
 
 require (
-	github.com/gorilla/websocket v1.4.2 // indirect
-	golang.org/x/crypto v0.4.0 // indirect
-	golang.org/x/sys v0.3.0 // indirect
+	github.com/danwakefield/fnmatch v0.0.0-20160403171240-cbb64ac3d964 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,220 +1,262 @@
 package main
 
 import (
-	"encoding/base64"
-	"flag"
+	"bufio"
+	browsercontext "discord-bot/browserContext"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"os/signal"
-	"strings"
-	"syscall"
-	"time"
 
-	"github.com/bwmarrin/discordgo"
+	"github.com/playwright-community/playwright-go"
 )
 
-var (
-	Token  string
-	remove string
-  disable string
-)
+// var (
+// 	Token   string
+// 	remove  string
+// 	addcmd  string
+// 	cmdDesc string
+// 	disable string
+// )
 
-func init() {
-	flag.StringVar(&Token, "t", "", "discord bot token")
-	flag.StringVar(&remove, "r", "", "remove command you want")
-  flag.StringVar(&disable, "d", "", "disable all comands")
-	flag.Parse()
+// func init() {
+// 	flag.StringVar(&Token, "t", "", "discord bot token")
+// 	flag.StringVar(&remove, "r", "", "remove command you want")
+// 	flag.StringVar(&disable, "d", "", "disable all comands")
+// 	flag.StringVar(&addcmd, "e", "", `add new command to system`)
+// 	flag.StringVar(&cmdDesc, "cd", "", "add description for that you newly created command")
+// 	flag.Parse()
 
-	if Token == "" {
-		flag.Usage()
-		return
-	}
+// 	if Token == "" {
+// 		flag.Usage()
+// 		return
+// 	}
 
-}
+// }
 
 func main() {
 
-	session, err := discordgo.New("Bot " + Token)
+	err := playwright.Install(&playwright.RunOptions{
+		Browsers: []string{"chromium"},
+		Verbose:  true,
+	})
 	if err != nil {
-		log.Fatalln("Couldn't start new session")
+		log.Fatalf("Couldn't install browser %v", err)
 	}
 
-	err = session.Open()
+	pw, err := playwright.Run(&playwright.RunOptions{
+		Browsers: []string{"chromium"},
+		Verbose:  true,
+	})
+
 	if err != nil {
-		log.Fatalln("Session couldn't opened")
-		return
+		log.Fatalf("Could Run playwight %v", err)
 	}
 
-	fmt.Println("Discord Bot running ctrl + c for kill")
+	browser, page := browsercontext.LaunchBrowser(pw, browsercontext.AuthTwitter, true)
+	loggedIn := browsercontext.IsLoggedin(page)
+	fmt.Printf("logged in %v", loggedIn)
 
-	session.AddHandler(messageHandler)
-	session.AddHandler(followupHandler)
+	for !browsercontext.IsLoggedin(page) {
 
+		cookies := <-browsercontext.Login(pw)
 
-	registeredCmds := make(map[string]*discordgo.ApplicationCommand, len(commands))
+		for _, cookie := range cookies {
+			if err := browser.AddCookies(playwright.BrowserContextAddCookiesOptionsCookies{
+				Name:     &cookie.Name,
+				Domain:   &cookie.Domain,
+				Value:    &cookie.Value,
+				Path:     &cookie.Path,
+				Expires:  &cookie.Expires,
+				HttpOnly: &cookie.HttpOnly,
+				Secure:   &cookie.Secure,
+				SameSite: &cookie.SameSite,
+			}); err != nil {
+				log.Fatalf("Couldn't set cookie to current context %v", err)
+			}
 
-	for _, v := range commands {
-		cmd, err := session.ApplicationCommandCreate(session.State.User.ID, "", v)
+		}
+
+		for _, cookie := range cookies {
+			fmt.Println(cookie.Name, cookie.Value)
+		}
+
+		_, err = page.Goto(browsercontext.Twitter)
 		if err != nil {
-			log.Fatalf("Couldn't recognized current command %v", err)
-			return
-		}
-		registeredCmds[v.Name] = cmd
-	}
-
-	removeCommand(session, registeredCmds)
-
-	defer session.Close()
-
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
-
-}
-
-func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-
-	msgs := strings.Fields(m.Content)
-	if len(msgs) <= 1 {
-		s.ChannelMessageSend(m.ChannelID, "args must be more than one")
-		return
-	}
-
-	for i, item := range msgs {
-		if strings.Contains(item, "https") {
-			// this is for eamil; handle for donwlaod image;r
-			avatar, err := getImage(item)
-			if err != nil {
-				log.Fatalf("an error accured try again later %v", err)
-			}
-
-			name := msgs[len(msgs)-1-i]
-
-			user, err := s.UserUpdate(name, avatar)
-			if err != nil {
-				log.Fatalf("Couldn't update user %v", err)
-			}
-
-			fmt.Printf("The user %s", user)
-
+			log.Fatalf("Couldnt go to %v err: %v", browsercontext.Twitter, err)
 		}
 
-	}
-
-}
-
-func getImage(url string) (string, error) {
-	var base64img string
-	var contentType string
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return "", err
-	}
-
-	defer func() {
-		_ = resp.Body.Close()
-	}()
-
-	img, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	contentType = http.DetectContentType(img)
-	base64img = base64.StdEncoding.EncodeToString(img)
-
-	return fmt.Sprintf("data:%s;base64,%s", contentType, base64img), nil
-
-}
-
-var (
-	commands = []*discordgo.ApplicationCommand{{
-		Name:        "followups",
-		Description: "Followups messages",
-	}}
-
-	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-		"followups": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Flags:   discordgo.MessageFlagsEphemeral,
-					Content: "Suprise!",
-				},
-			})
-			msg, err := s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
-				Content: "Follwup message has been created, after 5 seconds it will be edited",
-			})
-
-			if err != nil {
-				s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
-					Content: "Something went wrong",
-				})
-				return
-			}
-
-			time.Sleep(time.Second * 5)
-
-			content := "Now the original message is gone and after 10 seconds this message will ~~self-destruct~~ be deleted."
-			s.FollowupMessageEdit(i.Interaction, msg.ID, &discordgo.WebhookEdit{
-				Content: &content,
-			})
-
-			time.Sleep(time.Second * 10)
-			s.FollowupMessageDelete(i.Interaction, msg.ID)
-			s.FollowupMessageCreate(i.Interaction, true, &discordgo.WebhookParams{
-				Content: "For those, who didn't skip anything and followed tutorial along fairly, " +
-					"take a unicorn :unicorn: as reward!\n" +
-					"Also, as bonus... look at the original interaction response :D",
-			})
-
-		},
-	}
-)
-
-func followupHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
-
-	if hook, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
-		hook(s, i)
-	}
-
-}
-
-func removeCommand(s *discordgo.Session, appCmds map[string]*discordgo.ApplicationCommand) {
-
-	if remove == "" {
-		return
-	}
-
-	if cmd, ok := appCmds[remove]; ok {
-		err := s.ApplicationCommandDelete(s.State.User.ID, "", cmd.ID)
+		image, err := page.Screenshot()
 		if err != nil {
-			fmt.Printf("Couldn't Remove app cmd that id %v", err)
-			return
+			log.Fatalf("Couldt take screenshot form twitter %v", err)
 		}
+		ss, err := os.Create("ss.png")
+		if err != nil {
+			log.Fatalf("Couldn't create screenshot %v", err)
+		}
+		fw := bufio.NewWriter(ss)
+		fw.Write(image)
+		fmt.Println("all done !!")
 
-		fmt.Printf("Successfully command removed %v", cmd.Name)
 	}
 
+	if err := browser.ClearCookies(); err != nil {
+		log.Printf("Couldn't clear out the cookies %v", err)
+	}
+
+	if err := browser.Close(); err != nil {
+		log.Printf("Couldn't Shutdown the browser %v", err)
+	}
+	if err := os.RemoveAll("/tmp/twitter"); err != nil {
+		log.Printf("Couldn't remove /temp/twitter %v", err)
+	}
+
+	// session, err := discordgo.New("Bot " + Token)
+	// if err != nil {
+	// 	log.Fatalln("Couldn't start new session")
+	// }
+
+	// err = session.Open()
+	// if err != nil {
+	// 	log.Fatalln("Session couldn't opened")
+	// 	return
+	// }
+
+	// fmt.Println("Discord Bot running ctrl + c for kill")
+
+	// session.AddHandler(messageHandler)
+	// session.AddHandler(followupHandler)
+
+	// registeredCmds := make(map[string]*discordgo.ApplicationCommand, len(commands))
+
+	// for _, v := range commands {
+	// 	cmd, err := session.ApplicationCommandCreate(session.State.User.ID, "", v)
+	// 	if err != nil {
+	// 		log.Fatalf("Couldn't recognized current command %v", err)
+	// 		return
+	// 	}
+	// 	registeredCmds[v.Name] = cmd
+	// }
+	// removeCommand(session, registeredCmds)
+	// defer session.Close()
+
+	// sc := make(chan os.Signal, 1)
+	// signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	// <-sc
+
 }
 
-func disableCommands(s *discordgo.Session, appCmds map[string]*discordgo.ApplicationCommand) { 
-  if disable == "" {
-    log.Fatalf("Must be different than empty string")
-    return;
-  }
-  
-  test := "followups"
-  if cmd, ok := appCmds[test]; ok {
-    fmt.Printf("You're Valid to go %v", cmd.ApplicationID);
-   
-  }
+// func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+// 	if m.Author.ID == s.State.User.ID {
+// 		return
+// 	}
 
-}
+// 	msgs := strings.Fields(m.Content)
+// 	if len(msgs) <= 1 {
+// 		fmt.Println("must be bigger than one")
+// 		return
+// 	}
+
+// 	for i, item := range msgs {
+// 		if strings.Contains(item, "https") {
+// 			// this is for eamil; handle for donwlaod image;r
+// 			avatar, err := getImage(item)
+// 			if err != nil {
+// 				log.Fatalf("an error accured try again later %v", err)
+// 			}
+
+// 			name := msgs[len(msgs)-1-i]
+
+// 			user, err := s.UserUpdate(name, avatar)
+// 			if err != nil {
+// 				log.Fatalf("Couldn't update user %v", err)
+// 			}
+
+// 			fmt.Printf("The user %s", user)
+
+// 		}
+
+// 	}
+
+// }
+
+// func getImage(url string) (string, error) {
+// 	var base64img string
+// 	var contentType string
+
+// 	resp, err := http.Get(url)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	defer func() {
+// 		_ = resp.Body.Close()
+// 	}()
+
+// 	img, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	contentType = http.DetectContentType(img)
+// 	base64img = base64.StdEncoding.EncodeToString(img)
+
+// 	return fmt.Sprintf("data:%s;base64,%s", contentType, base64img), nil
+
+// }
+
+// var (
+// 	commands = []*discordgo.ApplicationCommand{{
+// 		Name:        "followups",
+// 		Description: "Followups messages",
+// 	}}
+
+// 	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+// 		"maker": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+// 			fmt.Println("make malive")
+// 		},
+// 	}
+// )
+
+// func followupHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+
+// 	if hook, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+// 		hook(s, i)
+// 	}
+
+// }
+
+// func removeCommand(s *discordgo.Session, appCmds map[string]*discordgo.ApplicationCommand) {
+
+// 	if remove == "" {
+// 		return
+// 	}
+
+// 	if cmd, ok := appCmds[remove]; ok {
+// 		err := s.ApplicationCommandDelete(s.State.User.ID, "", cmd.ID)
+// 		if err != nil {
+// 			fmt.Printf("Couldn't Remove app cmd that id %v", err)
+// 			return
+// 		}
+
+// 		fmt.Printf("Successfully command removed %v", cmd.Name)
+// 	}
+
+// }
+
+// func disableCommands(s *discordgo.Session, appCmds map[string]*discordgo.ApplicationCommand) {
+// 	if disable == "" {
+// 		log.Fatalf("Must be different than empty string")
+// 		return
+// 	}
+// 	test := "followups"
+// 	if cmd, ok := appCmds[test]; ok {
+// 		fmt.Printf("You're Valid to go %v", cmd.ApplicationID)
+// 	}
+// }
+
+// func blocker(s *discordgo.Session, m *discordgo.MessageCreate) bool {
+// 	if s.State.User.ID == m.Author.ID {
+// 		return false
+// 	}
+// 	return true
+// }
